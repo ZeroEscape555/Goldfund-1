d880ea733c6172baa12b779e1efc3a8ca9565c3c
AntoineYANG <814602569@qq.com>
2020-12-24 21:50:13 +0800

    published customerService.html

---
+++ react-app/src/App.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: Kanata You 
+ * @Date: 2020-12-23 19:38:02 
+ * @Last Modified by: Kanata You
+ * @Last Modified time: 2020-12-23 19:50:21
+ */
+
+import React from "react";
+import { CustomerService } from "./compos/CustomerService";
+
+
+const App: React.FC = () => {
+    return (
+        <CustomerService />
+    );
+};
+
+export default App;
---
+++ react-build/precache-manifest.467b811a501149b09a011ad04e052b2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b61ef6d395bb888b5d9fa4412503527",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1403fc1495e75ba88cb7",
+    "url": "/static/css/main.363d348f.chunk.css"
+  },
+  {
+    "revision": "cc345d67e7c2e2b275c7",
+    "url": "/static/js/2.8ec96b88.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.8ec96b88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1403fc1495e75ba88cb7",
+    "url": "/static/js/main.da8971a6.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
